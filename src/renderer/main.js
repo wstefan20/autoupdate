9c64308f235137fca95a8781ab84de2e8b305bd2
Stefan Wagner <wstefan20@gmail.com>
2019-08-06 17:53:04 -0500

    test

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,11 +4,9 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-import AppUpdater from './AppUpdater'
-// const log = require('electron-log');
-// const {autoUpdater} = require("electron-updater");
 
-// autoUpdater.logger = log;
+//
+
 // autoUpdater.logger.transports.file.level = 'info';
 // log.info('App starting...');
 
@@ -24,19 +22,28 @@ new Vue({
   template: '<App/>'
 }).$mount('#app')
 
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();
-// })
+if (process.env.NODE_ENV === 'production'){
+  app.on('ready', function()  {
+    const log = require('electron-log');
+    const {autoUpdater} = require("electron-updater");
+
+    autoUpdater.logger = log;
+    autoUpdater.logger.transports.file.level = 'info';
+    log.info('App starting...');
+
+    autoUpdater.checkForUpdates();
+    autoUpdater.on('checking-for-update', () => {
+    })
+    autoUpdater.on('update-available', (info) => {
+    })
+    autoUpdater.on('update-not-available', (info) => {
+    })
+    autoUpdater.on('error', (err) => {
+    })
+    autoUpdater.on('download-progress', (progressObj) => {
+    })
+    autoUpdater.on('update-downloaded', (info) => {
+      autoUpdater.quitAndInstall();
+    })
+  });
+}
