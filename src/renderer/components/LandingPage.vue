45e0486386b923fc612a3981e7457dba2269ba66
Stefan Wagner <wstefan20@gmail.com>
2019-08-06 16:52:49 -0500

    test

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          This is a Test!
         </span>
         <system-information></system-information>
       </div>
