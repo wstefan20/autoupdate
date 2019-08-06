e1ab61d18ac3649c75bb8c550b40e0abc74c4f15
Stefan Wagner <wstefan20@gmail.com>
2019-08-06 17:22:20 -0500

    testing auto update fix

--- src/main/index.js
+++ src/main/index.js
@@ -53,12 +53,12 @@ app.on('activate', () => {
  */
 
 
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
+// import { autoUpdater } from 'electron-updater'
+//
+// autoUpdater.on('update-downloaded', () => {
+//   autoUpdater.quitAndInstall()
+// })
+//
+// app.on('ready', () => {
+//   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+// })
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,13 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import AppUpdater from './AppUpdater'
+// const log = require('electron-log');
+// const {autoUpdater} = require("electron-updater");
+
+// autoUpdater.logger = log;
+// autoUpdater.logger.transports.file.level = 'info';
+// log.info('App starting...');
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
@@ -16,3 +23,20 @@ new Vue({
   store,
   template: '<App/>'
 }).$mount('#app')
+
+// app.on('ready', function()  {
+//   autoUpdater.checkForUpdates();
+// });
+// autoUpdater.on('checking-for-update', () => {
+// })
+// autoUpdater.on('update-available', (info) => {
+// })
+// autoUpdater.on('update-not-available', (info) => {
+// })
+// autoUpdater.on('error', (err) => {
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   autoUpdater.quitAndInstall();
+// })
