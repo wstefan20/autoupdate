08a29e0f211ae62b9b0f49ea686d745a41af8d41
Stefan Wagner <wstefan20@gmail.com>
2019-08-07 12:44:54 -0500

    test

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          One more time!
+          Please work!
         </span>
         <system-information></system-information>
       </div>
