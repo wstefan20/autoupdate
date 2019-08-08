761d0503da019ad63dca1d54ef00ec49247b56f9
Stefan Wagner <wstefan20@gmail.com>
2019-08-08 13:11:25 -0500

    test

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Please Please work!
+          I think it's working?
         </span>
         <system-information></system-information>
       </div>
